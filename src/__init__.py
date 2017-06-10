c60ad9311a5eb180814d1bc4ec68c7a202508b53
kpuhger <krpuhger@gmail.com>
2017-06-10 00:57:31 -0700

    modifying HEQ for z-stack

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# package imports
+import skimage as sk
+from skimage import io
+from skimage import feature
+from skimage import filters
+from skimage.morphology import disk
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
---
+++ src/smoothing.py
@@ -0,0 +1,22 @@
+# Mean blur. Increased disk_size = more smoothing
+def mean_blur(image, disk_size):
+    img_meanfilt = np.empty_like(image)
+    for i in range(len(image)):
+        img_meanfilt[i,:,:] = filters.rank.mean(image[i,:,:], disk(disk_size))
+    return img_meanfilt
+
+# Median blur. Increased disk_size = more smoothing
+def median_blur(image, disk_size):
+    img_medianfilt = np.empty_like(image)
+    for i in range(len(image)):
+        img_medianfilt[i,:,:] = filters.rank.median(image[i,:,:], disk(disk_size))
+    return img_medianfilt
+
+# Gaussian blur. Increased sigma = more smoothing
+def gaussian_blur(image, sigma):
+    # must convert image to float for this to work in a loop (unknown reason)
+    image = (image*1.0)/255.0
+    img_gaussian = np.empty_like(image)
+    for i in range(len(image)):
+        img_gaussian[i,:,:] = filters.gaussian(image[i,:,:], sigma)
+    return img_gaussian
